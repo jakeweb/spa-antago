646dc1de253b75eb864b35c3df512965ff8b4c3e
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-12-01 20:58:57 +0200

    move business logic to service

--- frontend/build/js/app.js
+++ frontend/build/js/app.js
@@ -1,11 +1,39 @@
 (function() {
-    angular.module("app", []).controller("appCtrl", appCtrl).directive("ngEnterDirective", ngEnterDirective);
+    angular.module("app", []).controller("appCtrl", appCtrl).service("appService", appService).directive("ngEnterDirective", ngEnterDirective);
 
-    appCtrl.$inject = ['$scope'];
+    appCtrl.$inject = ['$scope', 'appService'];
 
-    function appCtrl($scope) {
-        $scope.servicesCount = 0;
-        $scope.services = [{
+    function appCtrl($scope, appService) {
+        $scope.services = appService.services;
+        $scope.list = appService.list;
+
+        $scope.msgSend = function() {
+            appService.msgSend($scope.author, $scope.message);
+            $scope.message = '';
+        }
+    }
+
+    function appService() {
+        var servicesCount = 0;
+        var self = this;
+
+        self.list = [{
+            message: "Привет, Верунь! ниче себе ты крутая. фотка класс!!!!",
+            author: "Самуил",
+            date: "13 ноября 2011"
+
+        }, {
+            message: "Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент, это и есть всемирно известный центр огранки алмазов и торговли бриллиантами?",
+            author: "Лилия Семёновна",
+            date: "14 ноября 2011"
+
+        }, {
+            message: "Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент?",
+            author: "Лилия Семёновна",
+            date: "13 ноября 2011"
+
+        }];
+        self.services = [{
             name: "Ручное бронирование",
             count: "11",
             classColor: "progress__bar_green"
@@ -18,46 +46,25 @@
             count: "1",
             classColor: "progress__bar_blue"
         }];
+        self.msgSend = function(author, message) {
+            var date = new Date();
+            var feedback = {
+                message: message,
+                author: author,
+                date: date
+            }
+            self.list.push(feedback);
+        }
 
         // get total number
-        angular.forEach($scope.services, function(value, key) {
-            $scope.servicesCount += Number(value.count);
+        angular.forEach(self.services, function(value, key) {
+            servicesCount += Number(value.count);
         });
 
         // set the width of the progressbar
-        angular.forEach($scope.services, function(value, key) {
-            value.progressWidth = { "width": "" + (Number(value.count) / $scope.servicesCount) * 100 + "%" };
+        angular.forEach(self.services, function(value, key) {
+            value.progressWidth = { "width": "" + (Number(value.count) / servicesCount) * 100 + "%" };
         });
-
-        // list of the messages
-        $scope.list = [{
-            message: "Привет, Верунь! ниче себе ты крутая. фотка класс!!!!",
-            author: "Самуил",
-            date: "13 ноября 2011"
-
-        }, {
-            message: "Привет, Верунь! ниче себе ты крутая. фотка класс!!!!",
-            author: "Лилия Семёновна",
-            date: "13 ноября 2011"
-
-        }, {
-            message: "Привет, Верунь! ниче себе ты крутая. фотка класс!!!!",
-            author: "Лилия Семёновна",
-            date: "13 ноября 2011"
-
-        }];
-        $scope.msgSend = function() {
-            var date = new Date();
-            var feedback = {
-                message: $scope.message,
-                author: $scope.author,
-                date: date
-            }
-            $scope.list.push(feedback);
-            $scope.message = '';
-            $scope.author = '';
-        }
-
     }
 
     function ngEnterDirective() {
@@ -66,10 +73,13 @@
             link: function(scope, elem, attrs) {
                 elem.bind('keydown', function(event) {
                     var code = event.keyCode || event.which;
-                    if (code === 13 && event.ctrlKey) {
-                        event.preventDefault();
-                        scope.$apply(attrs.ngEnterDirective);
+                    if (scope.author && scope.message) {
+                        if (code === 13 && event.ctrlKey) {
+                            event.preventDefault();
+                            scope.$apply(attrs.ngEnterDirective);
+                        }
                     }
+
                 });
             }
         }
